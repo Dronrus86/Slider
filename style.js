7ac9f6e35811209f20b70d6ce59f8769e0882762
Dronrus86 <pop4666@yandex.ru>
2021-09-11 11:35:53 +0300

    html css js

--- style.js
+++ style.js
@@ -4,5 +4,43 @@ const form = document.getElementById('form');
 
 form.addEventListener("click", (e) =>{
     e.preventDefault();
-    alert("Вы точно хотите добавить товар вкорзину?");
   });
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+
+function nextSlide() {
+    showSlides(slideIndex += 1);
+}
+
+function previousSlide() {
+    showSlides(slideIndex -= 1);  
+}
+
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+
+function showSlides(n) {
+ 
+    let slides = document.getElementsByClassName("catalog_conditioners");
+    
+   
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+  
+
+   for (let slide of slides) {
+        slide.style.display = "none";
+    }
+  
+    slides[slideIndex - 1].style.display = "block";  
+  }
